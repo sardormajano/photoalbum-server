d82116f6bb6a24c70f23c5c635427f3d915f14b3
Sardorbek <sardor.majano@gmail.com>
2019-10-27 18:57:56 -0400

    #10 add cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.connect('mongodb://127.0.0.1:27017/photoalbum', {
   useNewUrlParser: true,
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/photoalbum', {
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.send('this is a root route'));
